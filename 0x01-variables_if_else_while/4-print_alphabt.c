d0736d250a81636aa2c6267079f299a7e13dcd6e
Yoda Mariam <myoda067@gmail.com>
2023-08-10 05:01:05 +0000

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+		putchar('\n');
+	return (0);
+}
