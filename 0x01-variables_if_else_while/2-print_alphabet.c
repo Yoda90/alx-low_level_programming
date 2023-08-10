8aa73cd0af8697fcafd6744b24610d64177c8861
Yoda Mariam <myoda067@gmail.com>
2023-08-10 04:13:48 +0000

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
