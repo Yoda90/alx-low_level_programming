f95f3c24dae17d01e5f0137371c7689c32f0deec
Yoda Mariam <myoda067@gmail.com>
2023-08-10 07:39:02 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int d = '0';
+
+
+	while (d <= '9')
+	{
+		putchar(d);
+
+		d++;
+
+		if (d <= '9')
+			putchar (',');
+
+
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
