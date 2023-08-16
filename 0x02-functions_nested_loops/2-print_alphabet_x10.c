39ffeca2e0b266cfee05fd0254818f6b0112a81c
Yoda Mariam <myoda067@gmail.com>
2023-08-16 00:40:07 +0000

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet a-z.
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{	int j, i;
+
+for (j = 1; j <= 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
+
+
+}
