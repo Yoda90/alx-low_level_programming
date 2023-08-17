2d816afd96fc14091aa9dd34caf274b507016880
Yoda Mariam <myoda067@gmail.com>
2023-08-17 20:08:31 +0000

    _isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper -  function that checks for uppercase character.
+ *@c:  c holds the character be checked
+ *
+ * Return: 1 if c is capital else 0
+ */
+int _isupper(int c)
+
+{
+	if isupper(c)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
