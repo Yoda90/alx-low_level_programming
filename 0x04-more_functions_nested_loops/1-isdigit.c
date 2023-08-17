2a67b7be1c73fc322ea1692e18de557d6f6781fa
Yoda Mariam <myoda067@gmail.com>
2023-08-17 20:19:29 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *  _isdigit -  function that checks for a digit (0 through 9).
+ *@c:  c holds the character be checked
+ *
+ * Return: 1 if c is checks for digit  else 0
+ */
+int _isdigit(int c)
+
+{
+	if isgigit(c)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
+
