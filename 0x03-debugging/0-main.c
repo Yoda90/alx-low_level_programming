5661640e21cb9c97c96ca8adf218101a4dfc4bd7
Yoda Mariam <myoda067@gmail.com>
2023-08-17 11:00:50 +0000

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	printf("%d is zero\n", n);
+	return (0);
+}
