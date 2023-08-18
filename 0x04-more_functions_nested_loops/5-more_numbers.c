7b7fae713f17f0c98d310a2bb693aaa1ee8efd0a
Yoda Mariam <myoda067@gmail.com>
2023-08-18 12:02:24 +0000

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,13 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 1; i <= 10; i++)
-
 	{
 		for (j = 0; j <= 14; j++)
-
-			_putchar(j);
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
 		_putchar('\n');
 	}
-	
 }
