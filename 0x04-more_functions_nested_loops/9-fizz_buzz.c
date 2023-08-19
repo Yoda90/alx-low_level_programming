bf14fa7b9aa16dfeb88434d0eaa6f0bcac9a3089
Yoda Mariam <myoda067@gmail.com>
2023-08-19 04:57:44 +0000

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf ("FizzBuzz");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf ("Fizz");
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf ("Buzz");
+		}
+		else 
+		{
+			printf ("%d", i);
+		}
+		if (i != 100)
+
+			printf (" ");
+		i++;
+	}
+	printf ("\n");
+	return 0;
+}
