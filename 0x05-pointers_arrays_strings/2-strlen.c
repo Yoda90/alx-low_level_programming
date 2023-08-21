099bb707bfa18c2a4bae2f3d40779c781ce98280
Yoda Mariam <myoda067@gmail.com>
2023-08-21 05:09:45 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - calculate length of a string.
+ *@s : holds length of a string.
+ *
+ *Return: 16
+ */
+int _strlen(char *s)
+{
+
+	printf("length of a %s\n", (s));
+
+	return (16);
+}
+
