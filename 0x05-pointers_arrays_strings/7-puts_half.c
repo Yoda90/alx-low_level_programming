61a138d6d3925d6ff7e2f442081f07eddee75455
Yoda Mariam <myoda067@gmail.com>
2023-08-21 12:33:12 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts_half - print half of a string.
+ *@str : holds the reverse of a string.
+ *
+ *Return:
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+
+	int i;
+	int middle = length / 2;
+
+	for (i = middle; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
