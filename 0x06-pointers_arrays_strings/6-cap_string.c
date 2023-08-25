f7f22050a91371bb273f07784b6b4d71bb0a862b
Yoda Mariam <myoda067@gmail.com>
2023-08-25 01:14:01 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,25 +3,26 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
-*cap_string - function that capitalizes all words of a string.
-*@ptr: is a strings.
-*
-*Return: ptr
-*/
+ *cap_string - function that capitalizes all words of a string.
+ *@ptr: is a strings.
+ *
+ *Return: ptr
+ */
 char *cap_string(char *ptr)
 {
 	size_t i;
 
-
 	for (i = 0; i < strlen(ptr); i++)
 	{
-		if (isspace(ptr[i - 1]) || ispunct(ptr[i - 1]))
+
+		if (isspace(ptr[i - 1]) || ispunct(ptr[i - 1]) || isblank(ptr[i - 1]))
 		{
 			if (islower(ptr[i]))
 			{
 				ptr[i] = toupper(ptr[i]);
 			}
 		}
+
 	}
 
 	return (ptr);
