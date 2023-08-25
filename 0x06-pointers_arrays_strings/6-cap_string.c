cd544de0c3031b423c0e514ee6867028c2dedcca
Yoda Mariam <myoda067@gmail.com>
2023-08-25 22:59:57 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -8,22 +7,37 @@
  *
  *Return: ptr
  */
+int is_separator(char c)
+{
+	int i;
+
+    char separators[] = " \t\n,;.!?\"(){}";
+    for ( i = 0; separators[i] != '\0'; ++i) {
+        if (c == separators[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 char *cap_string(char *ptr)
 {
-	size_t i;
+        size_t i;
 
-	for (i = 1; i < strlen(ptr); i++)
-	{
+        for (i = 0; i < strlen(ptr); i++)
+        {
+                if (is_separator(ptr[i - 1]) || i == 0)
+                {
+                        if (islower(ptr[i]))
+                        {
+                                ptr[i] = toupper(ptr[i]);
+                        }
+                }
+                else if (isupper(ptr[i]))
+                {
+                        ptr[i] = tolower(ptr[i]);
+                }
+        }
 
-		if (isspace(ptr[i - 1]) || ispunct(ptr[i - 1]) || isblank(ptr[i - 1]))
-		{
-			if (islower(ptr[i]))
-			{
-				ptr[i] = toupper(ptr[i]);
-			}
-		}
-
-	}
-
-	return (ptr);
+        return (ptr);
 }
