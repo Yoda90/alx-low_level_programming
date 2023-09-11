abb7c2752e29fe145631f8c868712a6e4c1889a6
Yoda Mariam <myoda067@gmail.com>
2023-09-11 23:03:26 +0000

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - free dogs
+ * @d: struct dog to print
+ */
+void free_dog(struct dog *d)
+{
+
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+	{
+		free(d);
+
+		return;
+	}
+}
