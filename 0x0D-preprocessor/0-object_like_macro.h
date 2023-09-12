34b24a54a56a5b1af151ac41ee39f700256a2f92
Yoda Mariam <myoda067@gmail.com>
2023-09-12 02:22:09 +0000

    second

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,7 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+int _putchar(char c);
+
 #define SIZE 1024
 #endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "0-object_like_macro.h"
 
 /**
  * main - prints the name of the file
  *
  * Return: 0 (SUCESS)
  */
-int main(void)
+int main()
 {
 	printf("%s\n", __FILE__);
-	return (0);
+	return(0);
+
 }
