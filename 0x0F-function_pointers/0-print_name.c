dddaf52603dbcdb694953601d9cc9428f36cd12f
Yoda Mariam <myoda067@gmail.com>
2023-09-13 20:05:14 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_name - function that prints a name
  * @name: name of the person
@@ -8,6 +8,11 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+
+	{
+		return;
+	}
 
 		f(name);
 
