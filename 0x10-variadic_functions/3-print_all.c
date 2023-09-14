4eeeb6393d329b8dca9fe0801eda5fe72dc8495f
Yoda Mariam <myoda067@gmail.com>
2023-09-14 20:03:40 +0000

    1-print_numbers.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print any number
+ * @format: its a string to a pointer
+ *
+ * Return:
+ */
+void print_all(const char * const format, ...)
+{
+	va_list allnumb;
+	int i = 0;
+	char *s = 0;
+	char *separator;
+
+	va_start(allnumb, format);
+
+	while (format && format[i])
+
+	{
+		switch (format[i])
+
+		{
+			case 'c':
+				printf("%s%c", separator,  va_arg(allnumb, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(allnumb, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(allnumb, double));
+				break;
+			case 's':
+				s = va_arg(allnumb, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+
+		separator = ", ";
+		i++;
+	}
+
+	printf("\n");
+	va_end(allnumb);
+}
