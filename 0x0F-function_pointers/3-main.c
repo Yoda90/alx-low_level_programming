6689ce2f5acb74fb712434a698d5f77fabe44576
Yoda Mariam <myoda067@gmail.com>
2023-09-14 02:08:29 +0000

    all

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,7 +4,6 @@
  * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: Always 0 (Sucess)
  */
 int main(int argc, char *argv[])
