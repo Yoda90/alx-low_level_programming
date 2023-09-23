6c91c5c75331750881f2855de7a1a268d18802ec
Yodaa90 <myoda067@gmail.com>
2023-09-23 03:04:28 +0000

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,30 +2,27 @@
 
 /**
  * add_node - adds a new node at the beginning
- * of a list.
+ * of a list_t list.
  * @head: head of the linked list.
- * @str: string to store  list.
+ * @str: string to store the list.
  *
  * Return: address of the head.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	size_t i;
+	size_t i = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	while (str[i])
+		i++;
+
 	new_node->str = strdup(str);
-
-	for (i = 0; str[i]; i++)
-
 	new_node->len = i;
-
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (*head);
