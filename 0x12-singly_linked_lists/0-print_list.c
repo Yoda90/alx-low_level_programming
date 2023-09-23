36b848a53a35a5d4dc3244c8633f38b0cbaeef8d
Yodaa90 <myoda067@gmail.com>
2023-09-23 00:42:26 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,16 +17,12 @@ size_t print_list(const list_t *h)
 	{
 
 		if (h->str == NULL)
-		{
 			printf("[0]  (nil)\n");
 
-		}
 		else
 
-		{
 			printf("[%u]  %s\n", h->len, h->str);
 
-		}
 		h = h->next;
 		number_count++;
 	}
